ed17e342ad37856c1566df5e09d25044c78677f2
Daniel <dana.gonzalezv@duocuc.cl>
2025-07-03 17:42:08 -0400

    se implemento HATEOAS

--- src/main/java/com/clientes/controllers/ClienteController.java
+++ src/main/java/com/clientes/controllers/ClienteController.java
@@ -3,8 +3,13 @@ package com.clientes.controllers;
 import com.clientes.dto.ClienteDTO;
 import com.clientes.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 import java.util.List;
 
@@ -25,12 +30,12 @@ public class ClienteController {
         return ResponseEntity.ok(service.listar());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ClienteDTO> obtener(@PathVariable Integer id) {
+   @GetMapping("/{id}")
+    public ResponseEntity<ClienteDTO> obtenerPorId(@PathVariable Integer id) {
         return service.obtenerPorId(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+}
 
     @PutMapping("/{id}")
     public ResponseEntity<ClienteDTO> actualizar(@PathVariable Integer id, @RequestBody ClienteDTO dto) {
@@ -45,4 +50,42 @@ public class ClienteController {
                 ? ResponseEntity.noContent().build()
                 : ResponseEntity.notFound().build();
     }
+
+
+        //METODO HATEOAS para buscar por ID
+    @GetMapping("/hateoas/{id}")
+    public ResponseEntity<ClienteDTO> obtenerHATEOAS(@PathVariable Integer id) {
+        return service.obtenerPorId(id)
+            .map(dto -> {
+                // Agregar los links HATEOAS
+                dto.add(linkTo(methodOn(ClienteController.class).obtenerHATEOAS(id)).withSelfRel());
+                dto.add(linkTo(methodOn(ClienteController.class).obtenerTodosHATEOAS()).withRel("todos"));
+                dto.add(linkTo(methodOn(ClienteController.class).eliminar(id)).withRel("eliminar"));
+
+                dto.add(Link.of("http://localhost:8888/api/proxy/productos/" + dto.getIdCliente()).withSelfRel());
+                dto.add(Link.of("http://localhost:8888/api/proxy/productos/" + dto.getIdCliente()).withRel("Modificar HATEOAS").withType("PUT"));
+                dto.add(Link.of("http://localhost:8888/api/proxy/productos/" + dto.getIdCliente()).withRel("Eliminar HATEOAS").withType("DELETE"));
+
+                return ResponseEntity.ok(dto);
+            })
+            .orElseGet(() -> ResponseEntity.notFound().build());
+}
+
+    //METODO HATEOAS para listar todos los productos utilizando HATEOAS
+    @GetMapping("/hateoas")
+    public List<ClienteDTO> obtenerTodosHATEOAS() {
+        List<ClienteDTO> lista = service.listar();
+
+        for (ClienteDTO dto : lista) {
+            //link url de la misma API
+            dto.add(linkTo(methodOn(ClienteController.class).obtenerHATEOAS(dto.getIdCliente())).withSelfRel());
+
+            //link HATEOAS para API Gateway "A mano"
+            dto.add(Link.of("http://localhost:8888/api/proxy/productos").withRel("Get todos HATEOAS"));
+            dto.add(Link.of("http://localhost:8888/api/proxy/productos/" + dto.getIdCliente()).withRel("Crear HATEOAS").withType("POST"));
+        }
+
+        return lista;
+    }
+
 }
